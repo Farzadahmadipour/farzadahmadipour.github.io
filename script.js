1dba5b2f6b353f9077fd08f3b30fe19caca5d82b
Farzad Ahmadipour Hedesh <farzad_ahmadipour@yahoo.com>
2025-11-06 17:44:52 +0330

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.querySelector(".menu-toggle");
-  const menu = document.querySelector("nav ul");
-
-  toggle.addEventListener("click", () => {
-    menu.classList.toggle("show");
-  });
-});
